b9d9835baa8ce148ec1d795694e4a77d68afb384
MyoMin <MayoMinPhai@gmail.com>
2023-05-10 13:35:24 -0400

    initial commit

---
+++ arrayScripts/2-28_sorting_array.js
@@ -0,0 +1,11 @@
+let kids = ["Natalie", "Brittany", "Zachary"];
+console.log(kids);
+kids.sort(); // Sorts the array
+
+
+let numKids = kids.length;
+for(let i = 0; i < numKids; i++) {
+ console.log(kids[i]);
+}
+
+   
